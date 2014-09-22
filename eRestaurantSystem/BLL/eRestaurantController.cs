906c5a51a5f8a55a8b37f83ed285cda0c7a35f48
Joseph Kennedy <jkennedy27@studentmail.nait.ca>
2014-09-22 15:58:34 -0600

    Class Demos Repo created from DWelchNAIT Repo

---
+++ eRestaurantSystem/BLL/eRestaurantController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.Entities;
+using eRestaurantSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class eRestaurantController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            //interfacing with our Context class
+            using(eRestaurantContext context = new eRestaurantContext())
+            {
+                return context.SpecialEvents.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Reservation> Reservation_List()
+        {
+            //interfacing with our Context class
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                return context.Reservations.ToList();
+            }
+        }
+
+    }
+}
